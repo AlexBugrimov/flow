68362d9f2b48c3320d49b1e54e15b2ff5f6e8eba
Alex <bugrimov_a@bk.ru>
2020-04-20 01:23:54 +0300

    [#2] Объектный тип

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 // @flow
 
-type User = {|
-  name: string,
-  age: number
-|}
-function save(user: User) {
-  return user;
-}
-save({
-  name: 'Alex',
-  age: 35
-})
+// SEALED - UNSEALED
+// SEALED object - Заполненный объект (добавить в него поля нельзя)
+const configSealed = {
+  db: 'test',
+  password: 'root'
+};
+// UNSEALED object - Пустой объект
+const config = {};
+const configUnsealed : {
+  [key: string] : number
+} = {};
+// Строковое поле (любое - например test) содержит числа
+config.test = 100;
